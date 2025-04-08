ae053e5d4486623d35510fcd64281e415cea3fc9
luiza <d.luiza@aluno.ifsp.edu.br>
2025-04-08 14:24:36 -0300

    metodo exibir-saldo

--- 004.py
+++ 004.py
@@ -12,4 +12,15 @@ class ContaBancaria:
             print("Erro: Saldo insuficiente!")
         else:
             self.__saldo -= valor
+
+    def exibir_saldo(self):
+        return self.__saldo
+
+if __name__ == "__main__":
+    c1 = ContaBancaria("Alice", 1000)
+    c1.depositar(500)
+    print("Saldo após depósito:", c1.exibir_saldo())
+    c1.sacar(2000)
+    c1.sacar(300)
+    print("Saldo final:", c1.exibir_saldo())
     
