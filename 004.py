51786545a250a3025f54ea2d29e92e032af57fac
luiza <d.luiza@aluno.ifsp.edu.br>
2025-04-08 14:23:40 -0300

    metodos depositar e sacar

--- 004.py
+++ 004.py
@@ -3,3 +3,13 @@ class ContaBancaria:
     def __init__(self, titular, saldo):
         self.__titular = titular
         self.__saldo = saldo
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        if valor > self.__saldo:
+            print("Erro: Saldo insuficiente!")
+        else:
+            self.__saldo -= valor
+    
